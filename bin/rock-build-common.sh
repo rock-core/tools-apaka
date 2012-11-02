25677dd38e90d18336de97ce5d73777c8c5d1992
Sylvain Joyeux <sylvain.joyeux@dfki.de>
2012-11-02 17:27:27 +0100

    display autoproj/autobuild versions before building for debugging reasons

--- bin/rock-build-common.sh
+++ bin/rock-build-common.sh
@@ -88,6 +88,8 @@ update() {
     if test "x$USE_PRERELEASE" = "xtrue"; then
 	gem install --prerelease autobuild autoproj
     fi
+    # To track which versions of autoproj/autobuild are used
+    autoproj --version
     autoproj full-build --no-color $COMMON_ARGS
 }
 
