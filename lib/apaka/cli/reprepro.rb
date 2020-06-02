0e334f487bf1d46416f15f45f718024e7398776f
Thomas Roehr <thomas.roehr@dfki.de>
2020-06-02 16:21:57 +0200

    cli: add reprepro cli

---
+++ lib/apaka/cli/reprepro.rb
@@ -0,0 +1,47 @@
+require_relative 'base'
+require_relative 'package'
+
+module Apaka
+    module CLI
+        class Reprepro < Base
+            def initialize
+                super()
+            end
+
+            def validate_options(args, options)
+                Base.activate_configuration(options)
+
+                args, options = Base.validate_options(args, options)
+                [:dest_dir, :base_dir, :patch_dir, :config_file, :log_dir].each do |path_option|
+                    Base.validate_path(options, path_option)
+                end
+
+                options[:architecture] = validate_architecture(options)
+                options[:distribution] = validate_distribution(options)
+                options[:release_name] ||= Packaging.default_release_name
+
+                return args, options
+            end
+
+            def run(args, options)
+                selected_packages = args
+                if options[:register]
+                    selected_packages.each do |pkg_name_expression|
+                        packager.reprepro.register_debian_package(pkg_name_expression,
+                                                           build_options[:release_name],
+                                                           build_options[:distribution])
+                    end
+                end
+
+                if options[:deregister]
+                    selected_packages.each do |pkg_name_expression|
+                        packager.reprepro.deregister_debian_package(pkg_name_expression,
+                                                           build_options[:release_name],
+                                                           build_options[:distribution])
+                    end
+                end
+
+            end
+        end
+    end
+end
